6d7060a5f67dfd60e9166a348772c6751ff99274
craigmw <craigm_walsh@yahoo.com>
2016-04-14 09:59:18 -0700

    Delete testLoop2.js

--- testLoop2.js
+++
@@ -1,9 +0,0 @@
-//testLoop2.js - test setInterval handle with looping.
-
-var tpHandle = null;
-
-tpHandle = setInterval( testFunction, 1000, tpHandle );
-
-function testFunction( tpHandle1 ) {
-    console.log( 'tpHandle: ' + JSON.stringify( tpHandle ) );
-};
